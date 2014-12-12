74a82dcaa244af178f5daf3f0d0ac7efe740b5f7
David Olsen <m@lude.net>
2014-12-12 18:32:30 -0500

    mongoize the scraper

--- scrape.py
+++ scrape.py
@@ -4,14 +4,17 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 import requests
 import re
+import pymongo
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.ncaa_tourney
 
 r = requests.get('http://espn.go.com/college-football/story/_/id/11419086/2014-15-college-football-bowl-schedule')
 soup = BeautifulSoup(r.text)
 
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
-
-bowl_games = []
 
 for x in soup.find_all('tr', attrs={'class':'last'}):
     game = {}
@@ -36,14 +39,14 @@ for x in soup.find_all('tr', attrs={'class':'last'}):
         m = p.match(teams[0])
         game['team0']['rank'] = m.group(1)
         game['team0']['name'] = m.group(2)
+    else:
+        game['team0']['name'] = teams[0]
+    if "No. " in teams[1]:
         m = p.match(teams[1])
         game['team1']['rank'] = m.group(1)
         game['team1']['name'] = m.group(2)
-    else:
-        game['team0']['name'] = teams[0]
+    else: 
         game['team1']['name'] = teams[1]
-    bowl_games.append(game)
+    db.games.insert(game)
     if "Florida Citrus Bowl" in game['location']:
         break
-
-pprint(bowl_games)
